ad792322db9c65cb36baf8855bf9198f5cf90e4d
Ruslan Yagudin <r@rusachello.xyz>
2019-12-25 00:50:17 +0300

    lesson 6

---
+++ startup_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+cd  ~/ && git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
