0d663a960dcef4f9b5a0727b64180a47b468e65c
Ruslan Yagudin <r@rusachello.xyz>
2020-01-27 03:10:14 +0300

    hw11 v1.1

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -28,14 +28,14 @@ resource "google_compute_instance" "app" {
     private_key = file(var.private_key_path)
   }
 
-  provisioner "file" {
-    source      = "../modules/app/files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "../modules/app/files/deploy.sh"
-  }
+#  provisioner "file" {
+#    source      = "../modules/app/files/puma.service"
+#    destination = "/tmp/puma.service"
+#  }
+#
+#  provisioner "remote-exec" {
+#    script = "../modules/app/files/deploy.sh"
+#  }
 }
 
 # Создание static IP
